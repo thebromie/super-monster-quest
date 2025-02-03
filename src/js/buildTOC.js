4de46f42f2c81a43bf258a295bbdbf3bffd8b842
Vinay Thamara <thebromie153@gmail.com>
2025-02-02 20:45:35 -0500

    Update variable types

--- src/js/buildTOC.js
+++ src/js/buildTOC.js
@@ -2,30 +2,30 @@ import { tableData } from "./tableData";
 
 function addChapterRow(id, item) {
   
-  let tocWrapper = document.getElementById("TOC-Wrapper");
+  const tocWrapper = document.getElementById("TOC-Wrapper");
 
-  let newChapter = document.createElement("div");
+  const newChapter = document.createElement("div");
   newChapter.setAttribute("id", item.chID);
   newChapter.setAttribute("class", "chapter-wrapper");
   
-  let chHeader = document.createElement("div");
+  const chHeader = document.createElement("div");
   chHeader.setAttribute("class", "chapter-header");
   
-  let linksWrapper = document.createElement("div");
+  const linksWrapper = document.createElement("div");
   linksWrapper.setAttribute("class", "links-wrapper");
 
 
-  let chIndex = document.createElement("span");
+  const chIndex = document.createElement("span");
   chIndex.setAttribute("class", "chapter-index");
   chIndex.innerHTML = "Chapter" + item.chIndex + ":";
 
-  let chTitle = document.createElement("span");
+  const chTitle = document.createElement("span");
   chTitle.setAttribute("class", "chapter-title");
   chTitle.innerHTML = item.chTitle;
 
   for (let i = 1; i <= item.pageTotal; i++) {
-    let newLink = document.createElement("a"); 
-    let paddedIndex = i.toString().padStart(3, '0')
+    const newLink = document.createElement("a");
+    const paddedIndex = i.toString().padStart(3, '0')
     newLink.setAttribute("href", "pageview.html?ch=" + id + "&pg=" + paddedIndex);
     newLink.innerHTML = paddedIndex
     linksWrapper.appendChild(newLink);
