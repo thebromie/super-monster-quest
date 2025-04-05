e45331b0b3476071008cc901c2885600839e7a4c
Vinay Thamara <thebromie153@gmail.com>
2025-04-05 00:12:13 -0400

    Refactor addChapterRow function for improved clarity and consistency

--- src/js/buildTOC.js
+++ src/js/buildTOC.js
@@ -1,49 +1,48 @@
-import { chapterData } from "./chapterData.js";
+import {chapterData} from "./chapterData.js";
 
-function addChapterRow(id, item) {
-  
-  const tocWrapper = document.getElementById("TOC-Wrapper");
+function addChapterRow(chapter) {
+    const tocWrapper = document.getElementById("TOC-Wrapper");
 
-  const newChapter = document.createElement("div");
-  newChapter.setAttribute("id", item.chID);
-  newChapter.setAttribute("class", "chapter-wrapper");
-  
-  const chHeader = document.createElement("div");
-  chHeader.setAttribute("class", "chapter-header");
-  
-  const linksWrapper = document.createElement("div");
-  linksWrapper.setAttribute("class", "links-wrapper");
+    const newChapter = document.createElement("div");
+    newChapter.setAttribute("id", `CH-${chapter.chIndex}`);
+    newChapter.setAttribute("class", "chapter-wrapper");
+
+    const chHeader = document.createElement("div");
+    chHeader.setAttribute("class", "chapter-header");
+
+    const linksWrapper = document.createElement("div");
+    linksWrapper.setAttribute("class", "links-wrapper");
 
 
-  const chIndex = document.createElement("span");
-  chIndex.setAttribute("class", "chapter-index");
-  chIndex.innerHTML = "Chapter" + item.chIndex + ":";
+    const chIndex = document.createElement("span");
+    chIndex.setAttribute("class", "chapter-index");
+    chIndex.innerHTML = "Chapter" + chapter.chIndex + ":";
 
-  const chTitle = document.createElement("span");
-  chTitle.setAttribute("class", "chapter-title");
-  chTitle.innerHTML = item.chTitle;
+    const chTitle = document.createElement("span");
+    chTitle.setAttribute("class", "chapter-title");
+    chTitle.innerHTML = chapter.chTitle;
 
-  for (let i = 1; i <= item.pageTotal; i++) {
-    const newLink = document.createElement("a");
-    const paddedIndex = i.toString().padStart(3, '0')
-    newLink.setAttribute("href", "pageview.html?ch=" + id + "&pg=" + paddedIndex);
-    newLink.innerHTML = paddedIndex
-    linksWrapper.appendChild(newLink);
-  }
+    for (let i = chapter.pageStart || 1; i <= (chapter.pageTotal + (chapter.pageStart || 0)); i++) {
+        const newLink = document.createElement("a");
+        const paddedIndex = i.toString().padStart(3, '0')
+        newLink.setAttribute("href", `pageview.html?ch=CH${chapter.chIndex}&pg=${paddedIndex}`);
+        newLink.innerHTML = paddedIndex
+        linksWrapper.appendChild(newLink);
+    }
 
-  chHeader.appendChild(chIndex);
-  chHeader.appendChild(chTitle);
-  newChapter.appendChild(chHeader);
-  newChapter.appendChild(linksWrapper);
+    chHeader.appendChild(chIndex);
+    chHeader.appendChild(chTitle);
+    newChapter.appendChild(chHeader);
+    newChapter.appendChild(linksWrapper);
 
-  tocWrapper.appendChild(newChapter);
+    tocWrapper.appendChild(newChapter);
 }
 
-function setupTableOfContents(){
-  for (const key in chapterData) {
-    addChapterRow(key, chapterData[key]);
-    // console.log(key + ": " + Object.keys(tableData).indexOf(key))
-  }
+function setupTableOfContents() {
+    const chapters = chapterData.sort((chA, chB) => chA.chIndex - chB.chIndex);
+    chapters.forEach((chapter) => {
+        addChapterRow(chapter);
+    });
 }
 
 setupTableOfContents();
